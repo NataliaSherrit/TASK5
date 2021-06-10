1677c4c4daf3c69492c8e7a879399df86ced22a1
NataliaSherrit <natalia.sherrit@gmail.com>
2021-06-10 21:34:17 +0300

    task 7: 1. Исправила замечания по задаче 5 (добавила getter к переменной result) 2. Добавила исключение при делении на 0

--- src/main/java/сalculator/Calculator.java
+++ src/main/java/сalculator/Calculator.java
@@ -4,28 +4,33 @@ package сalculator;
 public class Calculator {
    public static double result;
 
-    public Calculator(double num1, double num2, String operation) {
+    public Calculator(double num1, double num2, String operation) throws Exception {
 
-        switch (operation) {
-            case "+":
-                result = num1 + num2;
-                System.out.println("Результат равен " + result);
-                break;
-            case "-":
-                result = num1 - num2;
-                System.out.println("Результат равен " + result);
-                break;
-            case "*":
-                result = num1 * num2;
-                System.out.println("Результат равен " + result);
-                break;
-            case "/":
-                result = num1 / num2;
-                System.out.println("Результат равен " + result);
-                break;
-            default:
-                System.out.println("Операция не распознана.");
+            switch (operation) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        throw new Exception("На ноль делить нельзя");
+                    }
+                    else{
+                            result = num1 / num2;
+                        }
+                    break;
+                default:
+                    System.out.println("Операция не распознана.");
+            }
         }
 
+    public static double getResult() {
+        System.out.println("Результат равен " + result);
+        return result;
     }
 }
--- src/main/java/сalculator/Program.java
+++ src/main/java/сalculator/Program.java
@@ -2,11 +2,13 @@ package сalculator;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         double num1 = Numbers.getNum();
         double num2 = Numbers.getNum();
         String operation = Operation.getOperation();
-        Calculator result = new Calculator(num1,num2,operation);
+        Calculator calc = new Calculator(num1,num2,operation);
+        double result;
+        result = Calculator.getResult();
 
-}
+    }
 }
